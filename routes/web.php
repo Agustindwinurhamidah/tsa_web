c2aeaedd51d4ee963e4101abdca1a3710dddbeb5
= <agustinmida530@gmail.com>
2022-09-05 20:34:18 +0700

    routing web

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return 'SELAMAT DATANG';
+// });
+// Route::get('/home', function () {
+//     return view('welcome');
+// });
+ // Route::get('/home', function () {
+//     return view('welcome');
+// });
+// Route::get('/', [PageController::class, 'index']);
+
+// Route::get('/about', [PageController::class, 'about']);
+
+// Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// Route::get('/', [HomeController::class, 'index']);
+
+// Route::get('/about', [AboutController::class, 'index']);
+
+// Route::get('/articles/{id}', [ArticleController::class, 'index']);
+
+
+// // Route::resource('posts', PostController::class);
+
+// Route::get('/', function () {
+//     return 'SELAMAT DATANG';
+// });
+
+// Route::get('/about', function () {
+//     return 'NIM: 2241727001 <br> Nama: Agustin dwi nur hamidah <br> Kelas: Web-B';
+// });
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel ini dengan id '.$id;
 });
